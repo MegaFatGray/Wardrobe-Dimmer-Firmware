a3619e1aa260108eeca0fdb62900d1d51c33bd92
MegaFatGray <31743397+MegaFatGray@users.noreply.github.com>
2017-10-23 13:26:27 +0100

    Initial commit of blank Atmel Studio project

---
+++ Wardrobe-Dimmer-Firmware/Wardrobe-Dimmer-Firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * Wardrobe-Dimmer-Firmware.c
+ *
+ * Created: 23/10/2017 13:25:24
+ * Author : graym
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
