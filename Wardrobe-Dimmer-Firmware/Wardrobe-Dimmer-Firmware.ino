30cda18972e7256f830ad07340e4dd02d09d55cd
MegaFatGray <31743397+MegaFatGray@users.noreply.github.com>
2017-10-23 12:22:52 +0100

    Added ramping up to set point. Analog reading not integrated. On the fly dimmer adjustment not implemented. Problems with Arduino environment; takes long time to boot and start ramping & cannot initialise pin high quick enough (results in short flash of LEDs before turning off and ramping up).

--- Wardrobe-Dimmer-Firmware/Wardrobe-Dimmer-Firmware.ino
+++ Wardrobe-Dimmer-Firmware/Wardrobe-Dimmer-Firmware.ino
@@ -1,14 +1,40 @@
-#define potPin 1
-#define pwmPin 4
+#define potPin 1        // Potentiometer pin
+#define pwmPin 4        // PWM output pin
+#define rampTime 5000   // Time in ms to ramp up to PWM set point
+
+bool rampingFlag = true;                          // Represents if ramping is in progress
+int pwmSetPoint = 0;                              // Ramping PWM output
+int potReading = analogRead(potPin);              // Read potentiometer setting
+//int pwmMaxPoint = potReading / 4;                 // Scale down to 0->256 for max PWM set point
+int pwmMaxPoint = 200;
+uint32_t tickStart = millis();                    // Record ramping start time
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pwmPin, OUTPUT);
+  pinMode(pwmPin, OUTPUT);                          // Initialise PWM pin as output
+  digitalWrite(pwmPin, HIGH);                       // Initialise PWM pin high to hold mosfet off (LM358 comparator is inverting)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int potReading = analogRead(potPin);              // Read potentiometer setting
-  int pwmSetPoint = potReading / 4;                 // Scale down to 0->256 for PWM output
-  analogWrite(pwmPin, pwmSetPoint);                 // Set PWM output
+  if(rampingFlag)
+  {
+    uint32_t timeElapsed = (millis() - tickStart);          // Find time elapsed
+    if(timeElapsed >= rampTime)                             // If the ramp time has elapsed
+    {
+      rampingFlag = false;                                    // Clear flag to indicate ramping no longer in progress
+      pwmSetPoint = pwmMaxPoint;                              // Set pwm to max set point
+    }
+    else                                                    // Otherwise set pwm accordingly
+    {
+      pwmSetPoint = (timeElapsed*pwmMaxPoint) / rampTime;     // Scale to max set point
+    }
+  }
+  else
+  {
+    pwmSetPoint = pwmMaxPoint;
+  }
+  pwmSetPoint = pwmMaxPoint - pwmSetPoint;                  // Invert because LM358 comparator is inverting
+  pwmSetPoint += (256 - pwmMaxPoint);                       // Start ramping from 256 not max set point
+  analogWrite(pwmPin, pwmSetPoint);                         // Set PWM output
 }
